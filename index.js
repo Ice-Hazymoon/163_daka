5af21194259849949e4e111a11a2ded519d41d91
user3333444444 <90400981+user3333444444@users.noreply.github.com>
2021-09-09 22:28:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 用户名和密码
-const phone = '';
-const password = '';
+const phone = '13004972917';
+const password = 'lin@!12a';
 
 const rp = require('request-promise');
 const tough = require('tough-cookie');
